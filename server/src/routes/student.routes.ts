3ea8ab42e2df12ec66b8c14b9757bae394ee0291
samer <sameryousry99@gmail.com>
2026-08-07 20:11:27 +0300

    Add student internship wishes

--- server/src/routes/student.routes.ts
+++ server/src/routes/student.routes.ts
@@ -3,9 +3,14 @@ import { Router } from "express";
 
 import { getEligibleInternshipsHandler } from "../student/student-internship.controller";
 import { internshipPaginationSchema } from "../student/student-internship.schemas";
+import {
+  getStudentWishesHandler,
+  replaceStudentWishesHandler,
+} from "../student/student-wish.controller";
+import { replaceWishesSchema } from "../student/student-wish.schemas";
 import { authenticate } from "../middlewares/auth.middleware";
 import { requireRole } from "../middlewares/role.middleware";
-import { validateQuery } from "../middlewares/validate.middleware";
+import { validateBody, validateQuery } from "../middlewares/validate.middleware";
 
 export const studentRouter = Router();
 
@@ -15,3 +20,5 @@ studentRouter.get(
   validateQuery(internshipPaginationSchema),
   getEligibleInternshipsHandler,
 );
+studentRouter.get("/me/wishes", getStudentWishesHandler);
+studentRouter.put("/me/wishes", validateBody(replaceWishesSchema), replaceStudentWishesHandler);
---
+++ server/src/student/student-wish.controller.ts
@@ -0,0 +1,22 @@
+import type { RequestHandler } from "express";
+
+import type { ReplaceWishesInput } from "./student-wish.schemas";
+import { getStudentWishes, replaceStudentWishes } from "./student-wish.service";
+
+export const getStudentWishesHandler: RequestHandler = async (req, res) => {
+  const wishes = await getStudentWishes(req.user!.id);
+
+  res.status(200).json({
+    success: true,
+    data: { wishes },
+  });
+};
+
+export const replaceStudentWishesHandler: RequestHandler = async (req, res) => {
+  const wishes = await replaceStudentWishes(req.user!.id, req.body as ReplaceWishesInput);
+
+  res.status(200).json({
+    success: true,
+    data: { wishes },
+  });
+};
